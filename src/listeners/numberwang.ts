b44d13bdbbcf5b565833e2f5dc8372dec1d70c45
sploicers <sploicers@users.noreply.github.com>
2023-11-16 08:57:04 +1300

    Make numberwang a 10% ocurrence, rad bugfix

--- src/listeners/numberwang.ts
+++ src/listeners/numberwang.ts
@@ -1,6 +1,12 @@
 import { Robot } from "src/robot";
 
 export default function numberwang(robot: Robot) {
-    robot.hear(/^\d+$/, async ({ message, }) =>
-        await robot.react(message, 'numberwang'));
+	robot.hear(/^\d+$/, async ({ message, }) => {
+		const isNumberWang = tenPercentChance();
+		if (isNumberWang) {
+			await robot.react(message, 'numberwang');
+		}
+	});
 }
+
+const tenPercentChance = () => Math.floor(Math.random() * 10) === 0;
--- src/listeners/rad.ts
+++ src/listeners/rad.ts
@@ -2,5 +2,5 @@ import { Robot } from "src/robot";
 
 export default function rad(robot: Robot) {
     robot.hear(/\b(rad)\b/i, async ({ message, }) =>
-        await robot.react(message, 'call-me-hand'));
+        await robot.react(message, 'call_me_hand'));
 }
